435a2769d70884aff7d9d614fa4b35356a433ed4
kevin del cid <danieldelcidf@gmail.com>
2023-03-11 23:09:11 -0600

    Add new Logo and add elegant statusbar and head

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Text, View, Image } from 'react-native'
+import { Text, View, Image, StatusBar } from 'react-native'
 import { getImage } from '../../utils/images'
 
 export const Header = () => {
-  return (
-    <View style={{ backgroundColor:'black',  height:80, width:'100%', justifyContent: 'center', alignItems: 'center', marginBottom:15}}>
+  return (<>
+    <StatusBar backgroundColor="#444455" />
+    <View style={{ backgroundColor:'#444455',  height:80, width:'100%', justifyContent: 'center', alignItems: 'center', marginBottom:15}}>
         <Image source={getImage("luzmaya")} style={{   height:150, width:150, }}  resizeMode="contain" />
     </View>
+    </>
   )
 }
